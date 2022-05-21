84157523bbd3e8437218595b3fa3a318b22dbe30
Shmeeheart <Shmeeheart.goodwije@gmail.com>
2022-05-20 19:12:51 -0600

    Editing

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var tasks = {};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -8,13 +9,18 @@ var createTask = function(taskText, taskDate, taskList) {
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
+
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
+
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
+
   // if nothing in localStorage, create a new object to track all task status arrays
   if (!tasks) {
     tasks = {
@@ -24,6 +30,7 @@ var loadTasks = function() {
       done: []
     };
   }
+
   // loop over object properties
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
@@ -33,36 +40,48 @@ var loadTasks = function() {
     });
   });
 };
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
 });
+
 // modal is fully visible
 $("#task-form-modal").on("shown.bs.modal", function() {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
+
 // save button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
+
   if (taskText && taskDate) {
     createTask(taskText, taskDate, "toDo");
+
     // close modal
     $("#task-form-modal").modal("hide");
+
     // save in tasks array
     tasks.toDo.push({
       text: taskText,
       date: taskDate
     });
+
     saveTasks();
   }
 });
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -71,5 +90,7 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
 // load tasks for the first time
 loadTasks();
+
